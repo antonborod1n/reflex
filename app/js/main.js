330354bf1dc966e984bc71d5c10220fda34097d8
antonborod1n <antonborodin86@gmail.com>
2022-01-05 20:20:21 +0300

    added gallery, blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $ (function(){
+    $('.menu__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
 
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false
+    });
+
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.all'
+        }
+    });
 });
