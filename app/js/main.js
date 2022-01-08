9bdb3e91bcb059926d4ed111329d2ae0d0d65e24
antonborod1n <antonborodin86@gmail.com>
2022-01-08 10:30:00 +0300

    added contact page, without slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
+$(function () {
+    $('.lev__inner').slick({
+    });
+
+    $('.contact__slider-inner').slick({
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+    }); 
+});
+
 $ (function(){
     $('.menu__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close');
@@ -6,14 +18,10 @@ $ (function(){
         $('.rightside-menu').addClass('rightside-menu--close');
     });
 
-    $('.top__slider').slick({
-        dots: true,
-        arrows: false
-    });
-
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.all'
         }
     });
 });
+
